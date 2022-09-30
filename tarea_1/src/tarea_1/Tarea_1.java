65226a48433b647b54f63aa745de97de3191b370
jin-roh25 <57550291+jin-roh25@users.noreply.github.com>
2022-09-29 22:57:41 -0300

    Add files via upload

---
+++ tarea_1/src/tarea_1/Tarea_1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea_1;
+
+public class Tarea_1 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
