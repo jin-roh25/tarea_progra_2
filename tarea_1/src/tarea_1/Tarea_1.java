38281cd7179fd52df7153551344655b24dd68150
jin-roh25 <sebag2511@gmail.com>
2022-09-30 13:07:37 -0300

    proyecto base con clases

--- tarea_1/src/tarea_1/Tarea_1.java
+++ tarea_1/src/tarea_1/Tarea_1.java
@@ -5,31 +5,76 @@
  */
 package tarea_1;
 
-class Epiteto{
-    Epiteto(){
+class Pago{
+    Pago(){
         
     }
 }
 
-class Epiteta{
-    Epiteta(){
+class Efectivo extends Pago{
+    Efectivo(){
         
     }
 }
 
-public class Tarea_1 {
-
-    public static void main(String[] args) {
+class Transferencia extends Pago{
+    Transferencia(){
         
     }
-    
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package tarea_1;
+
+class Tarjeta extends Pago{
+    Tarjeta(){
+        
+    }
+}
+
+class DocTriburtario{
+    DocTriburtario(){
+        
+    }
+}
+
+class Boleta extends DocTriburtario{
+    Boleta(){
+        
+    }
+}
+
+class Factura extends DocTriburtario{
+    Factura(){
+        
+    }
+}
+
+class Dierccion{
+    Dierccion(){
+        
+    }
+}
+class Cliente{
+    Cliente(){
+        
+    }
+}
+
+class Articulo{
+    Articulo(){
+        
+    }
+}
+
+class DetalleOrden{
+    DetalleOrden(){
+        
+    }
+}
+
+class OrdenCompra{
+    OrdenCompra(){
+        
+    }
+}
 
 public class Tarea_1 {
 
