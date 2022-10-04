97d7a1e883fc463fb9bfee2c141d487c5fd26380
jin-roh25 <sebag2511@gmail.com>
2022-10-04 11:50:48 -0300

    DocTrivutario: metodos/{toString}

--- tarea_1/src/tarea_1/Tarea_1.java
+++ tarea_1/src/tarea_1/Tarea_1.java
@@ -4,6 +4,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tarea_1;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 class Pago{
     Pago(){
@@ -30,21 +32,61 @@ class Tarjeta extends Pago{
 }
 
 class DocTriburtario{
-    DocTriburtario(){
+    
+    private String numero;
+    private String rut;
+    private LocalDate fecha;
+    
+    DocTriburtario(String a, String b){
         
+        this.numero = a;
+        this.rut = b;
+        this.fecha = LocalDate.now();
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        
+    }
+    
+    public String getNumero(){
+        
+        return this.numero;
+    }
+    public String getRut(){
+        
+        return this.rut;
+    }
+    public String getFecha(){
+        
+        return this.fecha.format(myFormatObj);
+    }
+    public void setNumero(String a){
+        
+        this.numero =a;
+    }
+    public void setRut(String b){
+        
+        this.rut = b;
+    }
+    public void setFecha(){
+        
+        this.fecha = LocalDate.now();
+        myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     }
 }
 
 class Boleta extends DocTriburtario{
-    Boleta(){
+    Boleta(String num, String rut){
         
+        super(num,rut);
     }
+    
 }
 
 class Factura extends DocTriburtario{
-    Factura(){
+    Factura(String num, String rut){
         
+        super(num,rut);
     }
+    
 }
 
 class Dierccion{
